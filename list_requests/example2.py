5d25e7b4367900031ff311efb6bf6e2487f0aa94
glartir <sidorenko.slava@gmail.com>
2020-07-03 00:46:23 +0300

    added multiplied requests , rps

---
+++ list_requests/example2.py
@@ -0,0 +1,6 @@
+
+resp ={"1" : 5, "2" : 4}
+resp["1"]=8
+i=5
+resp[i] = 9
+print (resp)
--- list_requests/get_last_tickers.py
+++ list_requests/get_last_tickers.py
@@ -1,8 +1,10 @@
 from .base_request import BaseRequest
 from datetime import datetime, timedelta, timezone
-
+import threading
 import json
 import sys
+import time
+import pytest
 
 
 class GetLastTickers(BaseRequest):
@@ -25,10 +27,8 @@ class GetLastTickers(BaseRequest):
         size_content = sys.getsizeof(
             response.content) + sys.getsizeof(dict(response.headers))
 
-        max_size = 10 * 1024
-
-
-        assert size_content < max_size, "The size of the received data exceeds 10kb"
+        #бросаем исключение на 10*1024
+        assert size_content < 10240, "The size of the received data exceeds 10kb"
 
     def check_date(self, response):
         body = json.loads(response.text)
@@ -39,13 +39,14 @@ class GetLastTickers(BaseRequest):
             last_update = datetime.strptime(body['data'][i]["last_updated"], "%Y-%m-%dT%H:%M:%S.%f%z")
             dif_days = (server_time - last_update).days
 
-            assert dif_days == 0, "data not relevant"
+            assert dif_days == 0, "Data not relevant"
 
 
 
     def check_time(self, response):
-        assert response.status_code == 200, f"status code is NOT 200, response status = {response.status_code} "
-        assert response.elapsed.total_seconds() < 0.5, f"response time exceeds 500 ms, response time = {response.elapsed.total_seconds()}"
+        print(response.elapsed.total_seconds())
+        assert response.status_code == 200, f"Status code is NOT 200, response status = {response.status_code} "
+        assert response.elapsed.total_seconds() < 0.5, f"Response time exceeds 500 ms, response time = {response.elapsed.total_seconds()}"
 
     def first_task(self):
         response = self.get_10_last_tickers()
@@ -53,6 +54,52 @@ class GetLastTickers(BaseRequest):
         self.check_size(response)
         self.check_date(response)
 
+    def get_multuply_request(self,barrier,i,responses):
+
+        barrier.wait()                              # Накапливаем все запросы чтоб их запустить  в один момент
+        if responses["time_start"]==0:              # Первый вошедший поток запустит таймер
+            responses["time_start"]=time.time()
+
+        responses[i] = self.get_10_last_tickers()
+
+
+        # self.check_time(responses[i])
+        # self.check_size(responses[i])
+        # self.check_date(responses[i])
 
 
 
+
+
+
+
+
+    def multistart(self,number):
+        barrier = threading.Barrier(number,timeout=10)
+        running_threads=[]
+        responses = {"time_start" : 0, "time_finish" : 0}
+        for i in range (number):
+            thread=threading.Thread(target=self.get_multuply_request,args=(barrier,i,responses,))
+
+            thread.start()
+            running_threads.append(thread)
+        #time.sleep(10)
+        for r in running_threads:
+            r.join()
+        responses["time_finish"]=time.time()
+
+
+        for i in range(number):
+            self.check_time(responses[i])
+            self.check_size(responses[i])
+            self.check_date(responses[i])
+        total_time= responses["time_finish"]-responses["time_start"]
+        rps=number/total_time
+        assert  rps>5 , "The number of response per second is less than 5 "
+        print(f"total time = {total_time}")
+
+
+
+# a=GetLastTickers()
+# a.multistart()
+#a.first_task()
